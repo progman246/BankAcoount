7d0c1ae7bc519840738736734b3df183b16010e4
progman246 <hossein.abdi7@gmail.com>
2020-09-09 23:31:54 +0430

    Create project and define requireing files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
